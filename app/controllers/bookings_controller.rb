46bb669d3af31eb2c14d651efe09dd7b69b5ddee
Ekaterina Dzeraeva <katja.dzeraeva@gmail.com>
2022-06-07 12:32:14 +0200

    created bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,23 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
   def new
+    @user = current.user
+    @trip = Trip.find(params[:trip_id])
+    @booking = Booking.new(booking_params)
+
   end
 
   def create
+    @user = current.user
+    @trip = Trip.find(params[:trip_id])
+    @booking = Booking.new(booking_params)
+    @booking.user_id = @user.index
+    @booking.trip_id = @trip.id
+    if @booking.save
+      flash.alert = "Congratulations! You have booked a trip to #{@trip.destination}. "
+      redirect_to root_path
+    else
+    render :new
+    end
   end
 end
