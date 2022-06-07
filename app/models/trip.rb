9e233238ccdf7053213d3d750ecef96f358d07d1
Leo Prieto <leop22@outlook.fr>
2022-06-07 12:14:47 +0200

    Seeds created

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  belongs_to :company
+  belongs_to :company, optional: true
 
-  validates :destination, :arrival_date, :departure_date, :number_of_passengers, :descritpion, :price, :url_image, presence: true
-  validates :departure_date, comparison: { greater_than: :arrival_date }
+  validates :destination, :arrival_date, :departure_date, :number_of_passengers, :description, :price, :url_image, presence: true
+  # validates :departure_date, comparison: { greater_than: :arrival_date }
 end
