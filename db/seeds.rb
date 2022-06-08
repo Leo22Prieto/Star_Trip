50222c908528979ee283496c47129939d9deeeca
Leo Prieto <leop22@outlook.fr>
2022-06-08 10:29:55 +0200

    First commit Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,11 @@ Company.destroy_all
 puts 'Start the seeding'
 spacex = Company.create!(name: 'SpaceX', email: 'spacex@gmail.com', description: 'Voyagez sans mourir', rating: 5)
 
-pluton = Trip.create!(destination: 'Pluton', departure_date: '01-08-2022', arrival_date: '01-08-2023', number_of_passengers: 5, description: 'Profitez de la vue', price: 1000000, url_image: '#', company: spacex)
+pluton = Trip.create!(destination: 'Pluton', departure_date: '01-08-2022', arrival_date: '01-08-2023', number_of_passengers: 5, description: 'Profitez de la vue', price: 1000000, url_image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/pluto.jpg', company: spacex)
 
-mercure = Trip.create!(destination: 'Mercure', departure_date: '24-12-2023', arrival_date: '28-02-2030', number_of_passengers: 15, description: 'Comme dans Seul sur Mars maais sur Mercure', price: 580000, url_image: '#', company: spacex)
+mercure = Trip.create!(destination: 'Mercure', departure_date: '24-12-2023', arrival_date: '28-02-2030', number_of_passengers: 15, description: 'Comme dans Seul sur Mars maais sur Mercure', price: 580000, url_image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/mercury.jpg', company: spacex)
 
-venus = Trip.create!(destination: 'Venus', departure_date: '10-09-2024', arrival_date: '30-01-2025', number_of_passengers: 8, description: 'C\'est pas Venise', price: 290000, url_image: '#', company: spacex)
+venus = Trip.create!(destination: 'Venus', departure_date: '10-09-2024', arrival_date: '30-01-2025', number_of_passengers: 8, description: 'C\'est pas Venise', price: 290000, url_image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/332937/venus.jpg', company: spacex)
+
 
 puts 'Finished'
